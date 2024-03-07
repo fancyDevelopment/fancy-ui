89dc57e391cb7821d25bf469467e0901f3b7905e
Daniel Murrmann <daniel.murrmann@live.de>
2024-03-07 22:07:42 +0100

    Added exports to content lib

--- packages/content/src/index.ts
+++ packages/content/src/index.ts
@@ -1,9 +1,31 @@
+import { MetricComponent } from './lib/metric/metric.component';
+import { SwipeableTabsBladeComponent } from './lib/swipeable-tabs/swipeable-tabs-blade.component';
+import { SwipeableTabsComponent } from './lib/swipeable-tabs/swipeable-tabs.component';
+import { SwiperBladeComponent } from './lib/swiper/swiper-blade.component';
+import { SwiperComponent } from './lib/swiper/swiper.component';
+import { TabsItemComponent } from './lib/tabs/tabs-item.component';
+import { TabsComponent } from './lib/tabs/tabs.component';
 import { CardActionsComponent } from "./lib/card/card-actions.component";
 import { CardContentComponent } from "./lib/card/card-content.component";
 import { CardHeaderComponent } from "./lib/card/card-header.component";
 import { CardImageComponent } from "./lib/card/card-image.component";
-import { CardGrowComponent} from "./lib/card/card-grow.component";
+import { CardGrowComponent } from "./lib/card/card-grow.component";
 import { CardComponent } from "./lib/card/card.component";
 
-export { CardComponent, CardHeaderComponent, CardImageComponent, CardContentComponent, CardGrowComponent, CardActionsComponent };
-export const CardBundle = [ CardComponent, CardHeaderComponent, CardImageComponent, CardContentComponent, CardGrowComponent, CardActionsComponent ];
+export { 
+    CardComponent, 
+    CardHeaderComponent, 
+    CardImageComponent, 
+    CardContentComponent, 
+    CardGrowComponent, 
+    CardActionsComponent, 
+    TabsComponent, 
+    TabsItemComponent, 
+    SwiperComponent, 
+    SwiperBladeComponent, 
+    SwipeableTabsComponent, 
+    SwipeableTabsBladeComponent, 
+    MetricComponent 
+};
+
+export const CardBundle = [CardComponent, CardHeaderComponent, CardImageComponent, CardContentComponent, CardGrowComponent, CardActionsComponent];
